c179c9e0358557316e14cb590a629fb64d21be06
Tr0ya7 <DevLuizHBarros@proton.me>
2024-10-27 18:51:38 -0300

    Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import localFont from "next/font/local"
 import "./globals.css"
 import { ThemeProvider } from "@/components/provider/theme-provider"
+import { ConvexClientProvider } from "@/components/provider/convex-provider"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,9 +36,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="notion-theme-2">
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="notion-theme-2">
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://darling-drake-91.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
