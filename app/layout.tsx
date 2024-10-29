8d1c995e4df8510b7ffcdec60f8447003cde5439
Tr0ya7 <DevLuizHBarros@proton.me>
2024-10-29 17:41:47 -0300

    Schema & API

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,19 @@ import localFont from "next/font/local"
 import "./globals.css"
 import { ThemeProvider } from "@/components/provider/theme-provider"
 import { ConvexClientProvider } from "@/components/provider/convex-provider"
+import { Toaster } from "sonner"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
+  weight: "100 900"
+})
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 900",
-});
+  weight: "100 900"
+})
 
 export const metadata: Metadata = {
   title: "Notion Clone",
@@ -26,18 +28,15 @@ export const metadata: Metadata = {
       }
     ]
   }
-};
+}
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ConvexClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="notion-theme-2">
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -6,10 +6,13 @@ import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import UserItem from "../UserItem"
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
 
 const Navigation = () => {
     const isMobile = useMediaQuery("(max-width: 768px)")
     const pathname = usePathname()
+    const documents = useQuery(api.documents.get)
 
     const isResizingRef = useRef(false)
     const sidebarRef = useRef<HTMLDivElement>(null)
@@ -101,7 +104,11 @@ const Navigation = () => {
                     <UserItem />
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    {documents?.map((document) => (
+                        <p key={document._id}>
+                            {document.title}
+                        </p>
+                    ))}
                 </div>
                 <div 
                     className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" 
